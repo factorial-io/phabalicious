263ae7b41dac3d58416d83e475bd81f7580e6d70
Stephan Maximilian Huber <stephan@factorial.io>
2018-10-21 18:55:57 +0200

    Start with reset command

---
+++ src/Command/ResetCommand.php
@@ -0,0 +1,55 @@
+<?php /** @noinspection PhpRedundantCatchClauseInspection */
+
+namespace Phabalicious\Command;
+
+use Phabalicious\Exception\EarlyTaskExitException;
+use Phabalicious\Method\TaskContext;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ResetCommand extends BaseCommand
+{
+    protected static $defaultName = 'about';
+
+    protected function configure()
+    {
+        parent::configure();
+        $this
+            ->setName('reset')
+            ->setDescription('Resets the current application.')
+            ->setHelp('Reset the current application.');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int|null
+     * @throws \Phabalicious\Exception\BlueprintTemplateNotFoundException
+     * @throws \Phabalicious\Exception\FabfileNotFoundException
+     * @throws \Phabalicious\Exception\FabfileNotReadableException
+     * @throws \Phabalicious\Exception\MethodNotFoundException
+     * @throws \Phabalicious\Exception\MismatchedVersionException
+     * @throws \Phabalicious\Exception\MissingDockerHostConfigException
+     * @throws \Phabalicious\Exception\ShellProviderNotFoundException
+     * @throws \Phabalicious\Exception\TaskNotFoundInMethodException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($result = parent::execute($input, $output)) {
+            return $result;
+        }
+
+        $context = new TaskContext($this, $input, $output);
+
+        try {
+            $this->getMethods()->runTask('reset', $this->getHostConfig(), $context);
+        } catch (EarlyTaskExitException $e) {
+            return 1;
+        }
+
+        return $context->getResult('exitCode', 0);
+    }
+
+}
