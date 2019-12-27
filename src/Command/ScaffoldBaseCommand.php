d2fb5ae603c5f97ad0062c6f24184bdf79c7f30b
Stephan Huber <stephan@factorial.io>
2019-12-27 16:17:40 +0100

    Add workspace:update command and refactor scaffold code

--- src/Command/ScaffoldBaseCommand.php
+++ src/Command/ScaffoldBaseCommand.php
@@ -5,8 +5,12 @@
 namespace Phabalicious\Command;
 
 use Composer\Semver\Comparator;
+use InvalidArgumentException;
 use Phabalicious\Configuration\HostConfig;
+use Phabalicious\Exception\FabfileNotReadableException;
+use Phabalicious\Exception\FailedShellCommandException;
 use Phabalicious\Exception\MismatchedVersionException;
+use Phabalicious\Exception\MissingScriptCallbackImplementation;
 use Phabalicious\Exception\ValidationFailedException;
 use Phabalicious\Method\ScriptMethod;
 use Phabalicious\Method\TaskContextInterface;
@@ -15,11 +19,15 @@ use Phabalicious\ShellProvider\LocalShellProvider;
 use Phabalicious\Utilities\Utilities;
 use Phabalicious\Validation\ValidationErrorBag;
 use Phabalicious\Validation\ValidationService;
+use Phar;
+use RuntimeException;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 abstract class ScaffoldBaseCommand extends BaseOptionsCommand
 {
@@ -70,11 +78,11 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
      * @return int
      * @throws MismatchedVersionException
      * @throws ValidationFailedException
-     * @throws \Phabalicious\Exception\FabfileNotReadableException
-     * @throws \Phabalicious\Exception\FailedShellCommandException
-     * @throws \Phabalicious\Exception\MissingScriptCallbackImplementation
+     * @throws FabfileNotReadableException
+     * @throws FailedShellCommandException
+     * @throws MissingScriptCallbackImplementation
      */
-    protected function scaffold($url, $root_folder, TaskContextInterface $context)
+    protected function scaffold($url, $root_folder, TaskContextInterface $context, $tokens = [])
     {
         $is_remote = false;
         if (substr($url, 0, 4) !== 'http') {
@@ -87,7 +95,7 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
             $is_remote = true;
         }
         if (!$data) {
-            throw new \InvalidArgumentException('Could not read yaml from ' . $url);
+            throw new InvalidArgumentException('Could not read yaml from ' . $url);
         }
 
         if ($data && isset($data['requires'])) {
@@ -132,16 +140,15 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
             throw new ValidationFailedException($errors);
         }
 
-        $tokens = [
-            'uuid' => $this->fakeUUID(),
-        ];
+        $tokens['uuid'] = $this->fakeUUID();
+        if (isset($tokens['name'])) {
+            $tokens = Utilities::mergeData($this->readTokens($root_folder, $tokens['name']), $tokens);
+        }
 
         $questions = !empty($data['questions']) ? $data['questions'] : [];
-
-
         $tokens = $this->askQuestions($context->getInput(), $questions, $context, $tokens);
         if (empty($tokens['name'])) {
-            throw new \InvalidArgumentException('Missing `name` in questions, aborting!');
+            throw new InvalidArgumentException('Missing `name` in questions, aborting!');
         }
         if (!empty($data['variables'])) {
             $tokens = Utilities::mergeData($data['variables'], $tokens);
@@ -179,14 +186,15 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
         $context->set('loaderBase', $twig_loader_base);
 
         // Setup twig
-        $loader = new \Twig_Loader_Filesystem($twig_loader_base);
-        $this->twig = new \Twig_Environment($loader, array(
+        $loader = new Twig_Loader_Filesystem($twig_loader_base);
+        $this->twig = new Twig_Environment($loader, array(
         ));
 
 
         if (is_dir($tokens['rootFolder'])
             && empty($context->getInput()->getOption('force'))
             && empty($context->getInput()->getOption('override'))
+            && empty($tokens['allowOverride'])
         ) {
             if (!$context->io()->confirm(
                 'Destination folder exists! Continue anyways?',
@@ -208,17 +216,19 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
         /** @var CommandResult $result */
         $result = $context->getResult('commandResult');
         if ($result && $result->failed()) {
-            throw new \RuntimeException(sprintf(
+            throw new RuntimeException(sprintf(
                 "Scaffolding failed with exit-code %d\n%s",
                 $result->getExitCode(),
                 implode("\n", $result->getOutput())
             ));
         }
 
-        $context->io()->success('Scaffolding finished successfully!');
         if (!empty($data['successMessage'])) {
             $context->io()->block($data['successMessage'], 'Notes', 'fg=white;bg=blue', ' ', true);
         }
+        $this->writeTokens($tokens['rootFolder'], $tokens);
+
+        $context->io()->success('Scaffolding finished successfully!');
         return 0;
     }
 
@@ -261,7 +271,7 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
         $replacements = $this->getReplacements($tokens);
 
         if (empty($data[$data_key])) {
-            throw new \InvalidArgumentException('Scaffold-data does not contain ' . $data_key);
+            throw new InvalidArgumentException('Scaffold-data does not contain ' . $data_key);
         }
 
         $context->io()->comment(sprintf('Copying assets `%s`', $data_key));
@@ -276,7 +286,7 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
             if ($is_remote) {
                 $tmpl = $this->configuration->readHttpResource($data['base_path'] . '/' . $file_name);
                 if ($tmpl === false) {
-                    throw new \RuntimeException('Could not read remote asset: '. $data['base_path'] . '/' . $file_name);
+                    throw new RuntimeException('Could not read remote asset: '. $data['base_path'] . '/' . $file_name);
                 }
                 $tmp_target_file = '/tmp/' . $file_name;
                 if (!is_dir(dirname($tmp_target_file))) {
@@ -357,7 +367,9 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
             }
 
             $option_name = strtolower(preg_replace('%([a-z])([A-Z])%', '\1-\2', $key));
-            if (in_array($option_name, $this->dynamicOptions)) {
+            if (isset($tokens[$key])) {
+                $value = $tokens[$key];
+            } elseif (in_array($option_name, $this->dynamicOptions)) {
                 $value = $input->getOption($option_name);
             } else {
                 $value = $context->io()->ask(
@@ -368,7 +380,7 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
 
             if (!empty($question_data['validation'])) {
                 if (!preg_match($question_data['validation'], $value)) {
-                    throw new \InvalidArgumentException($question_data['error'] . ': ' . $value);
+                    throw new InvalidArgumentException($question_data['error'] . ': ' . $value);
                 }
             }
             if (!empty($question_data['transform'])) {
@@ -404,10 +416,25 @@ abstract class ScaffoldBaseCommand extends BaseOptionsCommand
      */
     protected function getLocalScaffoldFile($name)
     {
-        $rootFolder = \Phar::running()
-            ? \Phar::running() . '/config/scaffold'
+        $rootFolder = Phar::running()
+            ? Phar::running() . '/config/scaffold'
             : realpath(__DIR__ . '/../../config/scaffold/');
 
         return $rootFolder . '/' . $name;
+    }
+    
+    protected function writeTokens($root_folder, $tokens)
+    {
+        file_put_contents($root_folder . '/.phab-scaffold-tokens', YAML::dump($tokens));
+    }
+
+    protected function readTokens($root_folder, $name)
+    {
+        $full_path = "$root_folder/$name/.phab-scaffold-tokens";
+        if (!file_exists($full_path)) {
+            return [];
+        }
+        $tokens = yaml::parseFile($full_path);
+        return is_array($tokens) ? $tokens : [];
     }
 }
