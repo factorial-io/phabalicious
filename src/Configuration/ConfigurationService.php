dd30ed6f4294aa33c78d35ae7d551975aaf1aea7
Stephan Huber <stephan@factorial.io>
2020-10-09 10:52:43 +0200

    Show proper error-message if a remote asset cant be loaded

--- src/Configuration/ConfigurationService.php
+++ src/Configuration/ConfigurationService.php
@@ -342,7 +342,9 @@ class ConfigurationService
                 $add_data = $lookup[$resource];
             } elseif (strpos($resource, 'http') !== false) {
                 $add_data = Yaml::parse($this->readHttpResource($resource));
-                $this->checkRequires($add_data, $resource);
+                if ($add_data) {
+                    $this->checkRequires($add_data, $resource);
+                }
             } elseif (file_exists($root_folder . '/' . $resource)) {
                 $add_data = $this->readFile($root_folder . '/' . $resource);
             } else {
@@ -448,9 +450,9 @@ class ConfigurationService
         } elseif ($contents !== false) {
             file_put_contents($cache_file, $contents);
         }
-        if ($this->offlineMode && !$contents) {
+        if (!$contents) {
             $this->logger->error(
-                'Could not get needed data from offline-cache for `' .
+                'Could not get needed data from `' .
                 $resource . '`, proceed with caution!'
             );
         }
