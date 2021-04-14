98c15c71687e58489615da2790eb6181ba873d06
Stephan Huber <stephan@factorial.io>
2021-04-14 16:33:40 +0200

    Add support for named scripts (from the scripts section) in artifact based deployment

--- src/Artifact/Actions/Base/ScriptAction.php
+++ src/Artifact/Actions/Base/ScriptAction.php
@@ -9,6 +9,7 @@ use Phabalicious\Method\ScriptMethod;
 use Phabalicious\Method\TaskContextInterface;
 use Phabalicious\ShellProvider\CommandResult;
 use Phabalicious\ShellProvider\ShellProviderInterface;
+use Phabalicious\Utilities\Utilities;
 use Phabalicious\Validation\ValidationService;
 
 class ScriptAction extends ActionBase
@@ -22,6 +23,9 @@ class ScriptAction extends ActionBase
 
     protected function validateArgumentsConfig(array $action_arguments, ValidationService $validation)
     {
+        if (Utilities::isAssocArray($action_arguments['arguments'])) {
+            $validation->hasKey('name', 'if arguments is not a plain array please provide the name of the script');
+        }
     }
 
     protected function runImplementation(
@@ -37,9 +41,19 @@ class ScriptAction extends ActionBase
         /** @var ScriptMethod $script */
         $script = $context->getConfigurationService()->getMethodFactory()->getMethod('script');
 
+        $script_name  = $this->getArgument('name');
+        if (empty($script_name)) {
+            $script_data = $this->getArguments();
+        } else {
+            $script_data = $context->getConfigurationService()->findScript($host_config, $script_name);
+            if (!$script_data) {
+                throw new \InvalidArgumentException(sprintf("Could not find named script `%s`", $script_name));
+            }
+        }
+
         $cloned_context = clone $context;
         $cloned_context->set('rootFolder', $dir);
-        $cloned_context->set(ScriptMethod::SCRIPT_DATA, $this->getArguments());
+        $cloned_context->set(ScriptMethod::SCRIPT_DATA, $script_data);
         $cloned_context->set('variables', $context->get('deployArguments'));
 
         $saved = $script->getBreakOnFirstError();
