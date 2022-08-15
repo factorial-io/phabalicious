a2059b13a29006495ee867e7543f671c4de336ad
Stephan Huber <stephan@factorial.io>
2022-08-15 09:43:41 +0200

    docs: Add help for yarn and npm commands

--- src/Command/NpmCommand.php
+++ src/Command/NpmCommand.php
@@ -11,4 +11,41 @@ class NpmCommand extends SimpleExecutableInvocationCommand
     {
         parent::__construct($configuration, $method_factory, 'npm');
     }
+
+    protected function configure()
+    {
+        parent::configure();
+        $this->setHelp('
+Run a npm command in a specific context. Currently three contexts are supported:
+
+- the <info>host-context</info> (standard), which will execute npm in the same context as the
+  app
+- the <info>docker-host</info>-context, which will execute npm in the same context as where
+  docker commands executed (usually the parent)
+- the <info>docker-image</info>-context, which is piggypacking the script execution context and
+  run the command inside a docker-container derived from a given docker-image.
+  Suitable, when npm/ node is not available on the host.
+
+<options=bold>Configuration:</>
+Npm configuation is per host
+
+    hosts:
+      example:
+        additionalNeeds:
+          - npm
+        npm:
+          # the build command to execute on install and reset
+          buildCommand: build:prod
+          context: (host|docker-host|docker-image)
+          # for the docker-image context you can add additional configuration
+          # see script execution context for more info
+          image: node: 16
+        ...
+
+
+<options=bold>Examples:</>
+<info>phab -cconfig npm build</info>
+
+        ');
+    }
 }
