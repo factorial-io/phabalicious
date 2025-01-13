e51f4d9ae3b8b9803dd23755630fa76368211d84
Stephan Huber <phabalicious@factorial.io>
2025-01-13 16:57:09 +0100

    fix: Fix empty output lines

--- src/ShellProvider/KubectlShellProvider.php
+++ src/ShellProvider/KubectlShellProvider.php
@@ -140,7 +140,7 @@ class KubectlShellProvider extends LocalShellProvider
      */
     public function exists($file): bool
     {
-        return $this->run(sprintf('stat %s > /dev/null 2>&1', $file), RunOptions::NONE, false)
+        return $this->run(sprintf('stat %s > /dev/null 2>&1', $file), RunOptions::HIDE_OUTPUT, false)
             ->succeeded();
     }
 
